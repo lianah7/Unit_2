c292137d8814efe9871d8ccb5a5accec6f25be2f
liana <hilllia@ssfs.org>
2019-09-18 14:40:30 -0400

    Almost completed three problems

---
+++ sphere.py
@@ -0,0 +1,14 @@
+# by Liana Hill
+
+# problem one
+volume_of_sphere = 4 / 3 * 3.14 * 5 ** 3
+print("The volume of the sphere is", volume_of_sphere)
+
+# problem two
+num_books = 60
+book_cost = ((24.95 * 0.6) * num_books) + 3 + 0.75 * (num_books - 1)
+print("The wholesale cost is", book_cost)
+
+#problem three, not done
+running_time = (412 + (2 * 8.25) + (3 * 7.12)) / 60
+print("The time you get home is", running_time)
