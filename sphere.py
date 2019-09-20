4a88e9e3c9d3256cfaae123d3f9a22cbbed7254a
liana <hilllia@ssfs.org>
2019-09-20 08:45:53 -0400

    Finished three classwork problems

--- sphere.py
+++ sphere.py
@@ -9,6 +9,23 @@ num_books = 60
 book_cost = ((24.95 * 0.6) * num_books) + 3 + 0.75 * (num_books - 1)
 print("The wholesale cost is", book_cost)
 
-#problem three, not done
+# problem three, not done
 running_time = (412 + (2 * 8.25) + (3 * 7.12)) / 60
 print("The time you get home is", running_time)
+
+# problem 2 classwork
+length = 12
+width = 8
+area_of_rectangle = length * width
+print("The area of a rectangle is", area_of_rectangle)
+
+# problem 3 classwork
+triangle_height = 8
+base = 3
+area_of_triangle = triangle_height * (base/2)
+print("The area of a triangle is", area_of_triangle)
+
+# problem 4 classwork
+compound_interest = 10000 * (1 + (0.08/12)) ** (12 * 10)
+print("The compoun interest is", compound_interest)
+
