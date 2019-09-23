cf2002fb689e795374d673210631f42e4b74651d
liana <hilllia@ssfs.org>
2019-09-23 10:14:41 -0400

    started on unit 2 assignment option one

--- unit2project.py
+++ unit2project.py
@@ -2,5 +2,20 @@
 # last updated September 23, 2019
 # unit 2 assignment option one
 
-name = int(input("What is your name"))
-print("It's nice to meet you", name, "My name is ")
+name = input("What is your name?")
+print("It's nice to meet you", name, "My name is Karen from finance.")
+
+location = input("Where are you from?")
+print(location, "sounds like an awesome place to be from.")
+
+number = int(input("What is your favorite number?"))
+print("Your favorite number", number, "is", number/2, "times bigger than my favorite number (23)")
+
+car = input("What is your dream car?")
+print("That is such a cool car!")
+
+cost = float(input("How much does the car cost?"))
+print("That's expensive!")
+
+years = int(input("How many years will it take for you to pay off the car?"))
+print()
