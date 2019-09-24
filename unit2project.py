58c61c866d0b306546927d0fd39643221326e9f9
liana <hilllia@ssfs.org>
2019-09-24 11:15:44 -0400

    finished unit two option one assignment

--- unit2project.py
+++ unit2project.py
@@ -3,13 +3,13 @@
 # unit 2 assignment option one
 
 name = input("What is your name?")
-print("It's nice to meet you", name, "My name is Karen from finance.")
+print("It's nice to meet you", name, "My name is Emma from Engineering.")
 
 location = input("Where are you from?")
 print(location, "sounds like an awesome place to be from.")
 
 number = int(input("What is your favorite number?"))
-print("Your favorite number", number, "is", number/2, "times bigger than my favorite number (23)")
+print("Your favorite number", number, "is", number/2, "times bigger than my favorite number, 23!")
 
 car = input("What is your dream car?")
 print("That is such a cool car!")
@@ -18,4 +18,18 @@ cost = float(input("How much does the car cost?"))
 print("That's expensive!")
 
 years = int(input("How many years will it take for you to pay off the car?"))
-print()
+
+annual_interest = float(input("What is the annual interest rate of the car?"))
+
+monthly_interest = (annual_interest/100)/12
+
+number_of_monthly_payments = years * 12
+
+monthly_payments = (monthly_interest * cost) / (1 - (1 + monthly_interest) ** -(number_of_monthly_payments))
+total_cost = monthly_payments * (12 * years)
+
+print("Your monthly payment for a", str(car), "is", str(monthly_payments), "that is a total of", str(total_cost))
+
+
+
+
